582463111f79c645321e69d673fbe8a555237444
Rayan <anzairayan@gmail.com>
2022-07-16 16:41:03 -0300

    Commit inicial aula de Javascript

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var nome = "Rayan Anzai";
+var idade = 23;
+var idade2 = 10;
+//alert(nome + " tem " + idade + " anos!");
+//alert(idade + idade2);
+
+console.log(nome); //Para ver no console do navegador
+console.log(idade + idade2);
+
+
+var frase = "Japão é o melhor time do mundo";
+console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+
